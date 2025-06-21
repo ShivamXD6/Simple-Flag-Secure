7ec7a2a2ba6e774494c3a3b853de8dac4e2f91b8
Shivam <81681912+ShivamXD6@users.noreply.github.com>
2025-06-21 19:48:17 +0530

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -3,13 +3,16 @@
 # Abort in Recovery 
 if ! $BOOTMODE; then
   ui_print " ! Only uninstall is supported in recovery"
+  sleep 1
   ui_print " - Uninstalling Simple Flag Secure!"
+  sleep 1
+  ui_print " - You can report me @ShastikXD if the module gave bootloop"
+  sleep 1
   touch $MODPATH/remove
-  sh $MODPATH/uninstall.sh
   recovery_cleanup
   rm -rf $NVBASE/modules_update/$MODID $TMPDIR 2>/dev/null
   exit 0
 fi
 
-# Run Disable Script
+# Run Main Script
 "$MODPATH"/system/bin/bash "$MODPATH/disable.sh"
